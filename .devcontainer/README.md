98d92304aeace207e1d43fd09c898d4a47c9e25a
exilvm <exilvm@disroot.org>
2022-08-21 19:45:35 +0000

    add Devcontainer & Codespaces support

--- .devcontainer/README.md
+++
@@ -1,4 +0,0 @@
-# Node.JS + TypeScript + Python 3 along with their tools and editor extensions
-I use this container to develop projects that use a combination of Node.js, TypeScript and Python 3.
-
-Forked from: https://github.com/exilvm/dev-containers
--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,7 +1,18 @@
 #!/usr/bin/env bash
 
-# Update OS Packages
 sudo apt update && sudo apt -y upgrade
 
-# Install Dev Container CLI
-npm i @devcontainers/cli --global
+export DB_HOST=localhost DB_NAME=postgres DB_NAME_TEST=postgres DB_USER=postgres DB_PASSWORD=postgres PGPASSWORD=postgres DB_PORT=5432 DEBUG=manga-tracker:* TS_NODE_PROJECT=tsconfig.server.json TS_NODE_FILES=true ELASTIC_NODE=http://localhost:9200 BASE_URL=http://localhost:3000/api
+
+pip install --user -r requirements.txt
+
+npm i npm@latest @devcontainers/cli --global
+
+npm i
+
+npm run migrate:up 
+
+cd web
+
+npm i
+
